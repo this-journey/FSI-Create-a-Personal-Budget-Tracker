103ec7838ee20b0da35381407246751b9c71f749
Demi Sherae Zayas <iMac@This-Journey.home>
2021-11-22 19:41:01 -0800

    Finished copying

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-window.prompt (weeklyExpenseQuestions[0])
-let answer = window.prompt (weeklyExpenseQuestions[0])
-console.log (answer)
-let stringAnswer = window.prompt (weeklyExpenseQuestions[0])
-let numberAnswer = parseFloat (stringAnswer)
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
 console.log(numberAnswer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
