c6f5b44408b135367f32b97a92be4957858159af
Demi Sherae Zayas <iMac@This-Journey.home>
2021-11-22 19:24:19 -0800

    1st changes

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+window.prompt (weeklyExpenseQuestions[0])
+let answer = window.prompt (weeklyExpenseQuestions[0])
+console.log (answer)
+let stringAnswer = window.prompt (weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat (stringAnswer)
+console.log(numberAnswer)
